5e9b98c837dec7f23250a65665ae83c6ac471c5b
mikapchan <142721123+mikapchan@users.noreply.github.com>
2025-09-26 18:55:29 +0900

    script.js

--- script.js
+++ script.js
@@ -12,12 +12,29 @@ function updatePositions() {
   const vw = window.innerWidth;
   const vh = window.innerHeight;
 
-  hat.style.left = (vw/2 - 50) + 'px';
-  hat.style.top = (vh/3) + 'px';
+  // 帽子サイズ
+  const hatWidth = 150;
+  const hatHeight = 80;
 
-  spoon.style.left = (vw/2 - 30) + 'px';
-  spoon.style.top = (vh/2) + 'px';
+  // 小物サイズ
+  const spoonWidth = 80;
+  const spoonHeight = 80;
+
+  // 帽子を画面中央上寄せ
+  hat.style.position = 'absolute';
+  hat.style.width = hatWidth + 'px';
+  hat.style.height = hatHeight + 'px';
+  hat.style.left = (vw/2 - hatWidth/2) + 'px';
+  hat.style.top = (vh/3 - hatHeight/2) + 'px';
+
+  // 小物を画面中央下寄せ
+  spoon.style.position = 'absolute';
+  spoon.style.width = spoonWidth + 'px';
+  spoon.style.height = spoonHeight + 'px';
+  spoon.style.left = (vw/2 - spoonWidth/2) + 'px';
+  spoon.style.top = (vh*2/3 - spoonHeight/2) + 'px';
 
   requestAnimationFrame(updatePositions);
 }
+
 updatePositions();
